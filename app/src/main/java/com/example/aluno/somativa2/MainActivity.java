4b5b9c162851e4d59103d6bf76113136dbacc663
Adrianozk <adrianoluisf@gmail.com>
2018-04-02 22:55:10 -0300

    Ultimo update 10:54 pm em casa

--- app/src/main/java/com/example/aluno/somativa2/MainActivity.java
+++ app/src/main/java/com/example/aluno/somativa2/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.aluno.somativa2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -16,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private NumberPicker npNum;
     private Button btnSorteio;
     private Random aleatorio = new Random();
-    private int[] sorteado = new int[2];
-    private int novoNum;
+    private TextView txtPontuacao;
+    private int novoNum, sorteado1, sorteado2;
     private boolean repetiu = false;
 
     @Override
@@ -29,43 +31,41 @@ public class MainActivity extends AppCompatActivity {
         npNum = (NumberPicker) findViewById(R.id.npNum);
         btnSorteio = (Button) findViewById(R.id.btnSorteio);
 
+        txtPontuacao = (TextView) findViewById(R.id.txtPontuacao);
+
         npNum.setMaxValue(10);
         npNum.setMinValue(1);
 
         btnSorteio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (int j = 0; j < 1; j++) {
-                    //Atribui false para a variável boolean repetiu
-                    repetiu = false;
-                    //Faça
-                    do {
-                        //Atribui false para a variável boolean repetiu
-                        repetiu = false;
-                        //Gera o número aleatório de 0 a 99 e atribui para novoNum
-                        novoNum = aleatorio.nextInt(11);
-                        if(novoNum == 0)
-                            novoNum = 1;
-                        //laço de repetição incondicional
-                        for (int k = j - 1; k >= 0; k--)
-                            //Se o novo número aleatório é igual ao elemento da posição da matriz num
-                            if (novoNum == sorteado[k])
-                                //Atribui true para a variável boolean repetiu
-                                repetiu = true;
-                    } while (repetiu);//Enquanto repetiu
-                    //Atribui ao vetor o novo valor aleatório
-                    sorteado[j] = novoNum;
+                sorteado1 = aleatorio.nextInt(10);
+                sorteado1++;
+                sorteado2 = aleatorio.nextInt(10);
+                sorteado2++;
+                while (sorteado1 == sorteado2) {
+                    sorteado2 = aleatorio.nextInt(10);
+                    sorteado2++;
                 }
-                if(npNum.getValue() == sorteado[0] || npNum.getValue() == sorteado[1])
-                {
+                if (npNum.getValue() == sorteado1 || npNum.getValue() == sorteado2) {
                     Intent it = new Intent(MainActivity.this, Sorteio.class);
-                    it.putExtra("nDigitado",npNum.getValue());
-                    it.putExtra("sort1",sorteado[0]);
-                    it.putExtra("sort2",sorteado[1]);
+                    it.putExtra("nDigitado", npNum.getValue());
+                    it.putExtra("sort1", sorteado1);
+                    it.putExtra("sort2", sorteado2);
                     startActivity(it);
-                }else
-                    Toast.makeText(MainActivity.this,"Você não acertou o número sorteado",Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(MainActivity.this, "Você não acertou o número sorteado", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        //Utilização do SharedPreferences para ler a pontuação no arquivo de configuração
+        SharedPreferences leitor = getSharedPreferences("nome_config", MODE_PRIVATE);
+
+        Sorteio.pontuacao = leitor.getInt("pontuacao", 0);
+
+        txtPontuacao.setText("Pontuação: " + Sorteio.pontuacao);
     }
 }
--- app/src/main/java/com/example/aluno/somativa2/Sorteio.java
+++ app/src/main/java/com/example/aluno/somativa2/Sorteio.java
@@ -1,6 +1,7 @@
 package com.example.aluno.somativa2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,7 +10,7 @@ public class Sorteio extends AppCompatActivity {
 
     //Declaração de variáveis
     public static int pontuacao;
-    private TextView txtParabens, txtPontuacao, txtSort1, txtSort2, txtNum;
+    private TextView txtParabens, txtSort1, txtSort2, txtNum;
     private int sorteado1, sorteado2, nDigitado;
 
     @Override
@@ -17,24 +18,41 @@ public class Sorteio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sorteio);
 
-        pontuacao += 50;
         //Vinculando os componentes do layout com as variáveis referenciadas
         txtParabens = (TextView) findViewById(R.id.txtParabens);
-        txtPontuacao = (TextView) findViewById(R.id.txtPontuacao);
         txtSort1 = (TextView) findViewById(R.id.txtSort1);
         txtSort2 = (TextView) findViewById(R.id.txtSort2);
         txtNum = (TextView) findViewById(R.id.txtNum);
 
         //Pega os valores da outra tela através de Intent
-        sorteado1 = getIntent().getIntExtra("sort1",0);
-        sorteado2 = getIntent().getIntExtra("sort2",0);
-        nDigitado = getIntent().getIntExtra("nDigitado",0);
+        sorteado1 = getIntent().getIntExtra("sort1", 0);
+        sorteado2 = getIntent().getIntExtra("sort2", 1);
+        nDigitado = getIntent().getIntExtra("nDigitado", 0);
 
-        txtPontuacao.append(""+pontuacao);
-        txtSort1.append(""+sorteado1);
-        txtSort2.append(""+sorteado2);
-        txtNum.append(""+nDigitado);
+        txtSort1.append("" + sorteado1);
+        txtSort2.append("" + sorteado2);
+        txtNum.append("" + nDigitado);
 
+    }
 
+    protected void onPause()
+    {
+        super.onPause();
+        //Utilização do SharedPreferences para gravar a pontuação no arquivo de configuração
+        SharedPreferences.Editor gravar = getSharedPreferences("nome_config", MODE_PRIVATE).edit();
+
+        gravar.putInt("pontuacao", pontuacao);
+
+        gravar.commit();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        pontuacao = 50;
+        //Utilização do SharedPreferences para ler a pontuação no arquivo de configuração
+        SharedPreferences leitor = getSharedPreferences("nome_config", MODE_PRIVATE);
+
+        int p = leitor.getInt("pontuacao", 0);
+        pontuacao += p;
     }
 }
